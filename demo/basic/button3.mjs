42ad1fb1eb5ea9b69b557e6c2c5a59a2f9e405cf
Luke Aaron <luke@devanew.com>
2023-04-27 05:01:06 +0100

    tidied state output demo

--- demo/basic/button3.mjs
+++ demo/basic/button3.mjs
@@ -2,9 +2,14 @@ export default {
     style: v => (`
         button {
             font-size: 10px;
+            margin: 10px;
+            border: 0;
+            border-radius: 5px;
+            padding: 2px 5px;
+            box-shadow: 0 0 5px #0000000f;
         }
      `),
-    view: v => (`<button>${v.watch('count', v => (act.get().count))}</button>`),
+    view: v => (`<button>Count state: ${v.watch('count', v => (act.get().count))}</button>`),
     script: v => {
 
         v.on = {
